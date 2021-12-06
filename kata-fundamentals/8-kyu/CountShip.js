b1c81f850514cd9abf1dd35d11b5c0f465f09852
ZeraiGR <kirillgr15@mail.ru>
2021-12-06 09:15:56 +0300

    day 15 "CountShip"

---
+++ kata-fundamentals/8-kyu/CountShip.js
@@ -0,0 +1,29 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+// My solution
+function countSheeps(arrayOfSheep) {
+    if (Array.isArray(arrayOfSheep)) {
+        const totalSheeps = arrayOfSheep.filter(
+            (el) => typeof el == 'boolean' && el == true
+        ).length;
+        return totalSheeps;
+    }
+}
+
+// Best practice
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+}
